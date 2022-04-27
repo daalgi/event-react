ac558e485d2748794a72deb152753d7b4f6f514c
daalgi <daalgicam@gmail.com>
2022-04-27 23:12:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useCallback } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [count, setCount] = useState(0)
+
+    // const handleEvent = useCallback(e => {
+    //     setCount(e.detail.count)
+    //     console.log("Inside handleEvent function")
+    // })
+    const handleEvent = e => {
+        setCount(e.detail.count)
+        console.log("Inside handleEvent function")
+    }
+
+    useEffect(() => {
+        document.addEventListener(
+            "external-btn-custom-event", handleEvent)
+
+        // Remove event listener to avoid memory leaks
+        return () => {
+            document.removeEventListener(
+                "external-btn-custom-event", handleEvent)
+        }
+    }, [handleEvent])
+
+    console.log("React page render")
+    return <h3>React external button clicks count: {count}</h3>
+
 }
 
 export default App;
